154b06b64b21f31f7bfdae611a283e1bb8b784d1
Barata <33399712+baratoxis@users.noreply.github.com>
2022-01-22 14:59:42 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -246,7 +246,7 @@ Write-Host 'Done.'
 
 write-host @'
 ***************** 
-It was rough. It was so painful. I was so scared. I was so sad. It hurt so much, I thought I’d die. I tried so hard… I tired so hard! I was so desperate, so desperate to make everything right! It’s the truth. I’ve honestly never tried so hard at anything in my life! Because I loved this place… Because it was so important to me! I was so desperate to get back. I was scared… I was so scared! I didn’t want those eyes looking at me like that again… And I hated myself so much for feeling that way!
+"It was rough. It was so painful. I was so scared. I was so sad. It hurt so much, I thought I’d die. I tried so hard… I tired so hard! I was so desperate, so desperate to make everything right! It’s the truth. I’ve honestly never tried so hard at anything in my life! Because I loved this place… Because it was so important to me! I was so desperate to get back. I was scared… I was so scared! I didn’t want those eyes looking at me like that again… And I hated myself so much for feeling that way!"
 ***************** 
 '@
 
